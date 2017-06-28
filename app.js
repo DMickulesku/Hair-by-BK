62661a0ba0a459b9a40daafd8f6feb9796f9d96d
DMickulesku <danmickulesku@gmail.com>
2017-06-28 11:46:22 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+emailjs.init("user_MOJnKnOPCqvRmQvDWuz8s");
+
+$(function() {
+
+  $('.submitbtn').on('click', function(event) {
+    event.preventDefault()
+
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var phoneNumber = $('#phoneNumber').val();
+    var email = $('#email').val();
+    var serviceRequested = $('#serviceRequested').val();
+    var name = firstName + ' ' + lastName;
+
+    var obj = {}
+    obj.to_name = name
+    obj.from_name = name
+    obj["phone_number"] = phoneNumber
+    obj.email = email
+    obj["services_requested"] = serviceRequested
+
+    console.log(obj);
+    emailjs.send("danmickulesku_gmail_com", "template_u36WH5zW", obj);
+
+    $.post("https://cors-anywhere.herokuapp.com/https://guarded-dawn-58914.herokuapp.com/api/v1/clients/", obj)
+      .then(function() {
+
+
+      })
+  })
+
+
+
+  // Initialize modal
+  $('.modal').modal(
+
+  );
+});
